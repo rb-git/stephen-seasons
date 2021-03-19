81db2530b9ee77124135b0d28b67bdaa11d3aabd
unknown <rohit_bapat@hotmail.com>
2021-03-19 15:47:15 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import "semantic-ui-css/semantic.min.css";
+import Spinner from "./Spinner";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component {
+  //IMP - state is always an object with K-V
+  state = { lat: null, errorMessage: "" };
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        //STATE UPDATE uses this.setState(JS obj with KV)
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  }
+
+  renderContent() {
+    //state reference always done as this.state.property
+    if (this.state.lat && !this.state.errorMessage) {
+      return (
+        <div>
+          <SeasonDisplay lat={this.state.lat} />
+        </div>
+      );
+    }
+    if (!this.state.lat && this.state.errorMessage) {
+      return <div>Error Message: {this.state.errorMessage} </div>;
+    }
+    return (
+      <div>
+        <Spinner message="please accept location request" />
+      </div>
+    );
+  }
+
+  render() {
+    return <div>{this.renderContent()}</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
